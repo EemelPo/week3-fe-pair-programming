26f14d048a8308d97c4732ebbc20f9003d358396
Eemeli <eemelpo@metropolia.fi>
2024-09-04 11:09:29 +0300

    wednesday pair programming submission

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,9 +1,50 @@
+import PageLinks from './PageLinks';
+import logo from '../images/logo.svg'
 const Navbar = () => {
   return (
-    <div>
-      <p className="section-dummy">About</p>
-      <p className="section-dummy">...</p>
+    <nav className="navbar">
+    <div className="nav-center">
+      <div className="nav-header">
+        <img src={logo} className='nav-logo' alt='backroads' />
+        <button type="button" className="nav-toggle" id="nav-toggle">
+          <i className="fas fa-bars"></i>
+        </button>
+      </div>
+      <PageLinks parentClass='nav-links' itemClass='nav-link' />
+      <ul className="nav-icons">
+        <li>
+          <a
+            href="https://www.twitter.com"
+            target="_blank"
+            rel="noreferrer"
+            className="nav-icon"
+          >
+            <i className="fab fa-facebook"></i>
+          </a>
+        </li>
+        <li>
+          <a
+            href="https://www.twitter.com"
+            target="_blank"
+            rel="noreferrer"
+            className="nav-icon"
+          >
+            <i className="fab fa-twitter"></i>
+          </a>
+        </li>
+        <li>
+          <a
+            href="https://www.twitter.com"
+            target="_blank"
+            rel="noreferrer"
+            className="nav-icon"
+          >
+            <i className="fab fa-squarespace"></i>
+          </a>
+        </li>
+      </ul>
     </div>
+  </nav>
   );
 };
 
--- src/components/Tours.js
+++ src/components/Tours.js
@@ -1,9 +1,20 @@
+import Tour from './Tour';
+import { tours } from '../data';
 function Tours() {
   return (
-    <div>
-      <p className="section-dummy">Tours</p>
-      <p className="section-dummy">...</p>
-    </div>
+    <section className="section" id="tours">
+  <div className="section-title">
+    <h2>
+      featured <span>tours</span>
+    </h2>
+  </div>
+  <div className="section-center featured-center">
+    {tours.map((tour) => (
+      <Tour key={tour.id} {...tour} />
+    ))}
+  </div>
+</section>
+
   );
 }
 
